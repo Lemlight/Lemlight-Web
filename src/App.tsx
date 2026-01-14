5e6fa36ecec192e29b9df7d0cabf60ea67d199ff
Godwin Daniel <ydkdan6@gmail.com>
2026-01-14 14:20:58 +0100

    fixing undeclared routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { ROUTES } from "./routes/routeConstants";
+// import { ROUTES } from "./routes/routeConstants";
 
 const queryClient = new QueryClient();
 
